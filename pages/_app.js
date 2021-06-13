eac3e30f007c4dbd0230d9b77bf0762702247a5f
Jose Duarte <joseduarte@joses-air.home>
2021-06-13 12:36:22 -0700

    new testing branch

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/globals.css';
 
+import { ThemeProvider } from 'next-themes';
+
 import CartState from 'context/cart/CartState';
 import CheckoutState from 'context/checkout/CheckoutState';
 
@@ -7,7 +9,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <CheckoutState>
       <CartState>
-        <Component {...pageProps} />
+        <ThemeProvider attribute='class'>
+          <Component {...pageProps} />
+        </ThemeProvider>
       </CartState>
     </CheckoutState>
   );
